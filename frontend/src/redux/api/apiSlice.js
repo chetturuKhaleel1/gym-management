9e81c51c26af7f01fdcb8f3152396a4204af9e87
chetturuKhaleel1 <khaleelchetturu@gmail.com>
2025-09-04 14:41:03 +0530

    Update apiSlice.js

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,9 +1,11 @@
 // src/redux/api/apiSlice.js
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const BASE_URL = "https://gym-management-backend-0tn2.onrender.com/api";
+
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: BASE_URL,
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.userInfo?.token;
       if (token) {
